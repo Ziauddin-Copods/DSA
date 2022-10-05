ff10d6cc3eb4acf90ea3a0077d38a7305e0ec987
Ziauddin <ziauddin@copods.co>
2022-10-05 10:53:09 +0530

    New power function added

--- Recursion/power.js
+++ Recursion/power.js
@@ -4,4 +4,14 @@ function pow(m, n) {
     return pow(m,n-1)*m;
 }
 
-console.log(pow(2,9))
+function pow2(m, n) {
+    if (n === 0) return 1;
+
+    if (n % 2 === 0) {
+        return pow2(m * m, n / 2);
+    }
+
+    return m * pow2(m * m, (n - 1) / 2);
+}
+
+console.log(pow2(2,9))
